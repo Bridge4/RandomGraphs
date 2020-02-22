e7b0a9edc7eb857278498902a79bbb356158f1ab
Bridge4 <33097098+Bridge4@users.noreply.github.com>
2020-02-21 19:34:59 -0700

    Testing

--- random_graphs.py
+++ random_graphs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def generate_rand_graph(number_of_vertices):
-    generated_graph = nx.Graph()
+    generated_graph = nx.Graph() 
     # generating an edge between each random number
     for i in range(0, int(number_of_vertices)):
         key = rand.randrange(0, number_of_vertices)
@@ -16,10 +16,10 @@ def generate_rand_graph(number_of_vertices):
 def main():
     while True:
         vert_count = int(input("Enter number of vertices: "))
-        graph = generate_rand_graph(vert_count)
-        nx.draw(graph)
-        plt.savefig("generated_graph.png")
-        plt.show()
+        graph = generate_rand_graph(vert_count) # generates graph
+        nx.draw(graph) #draws it
+        plt.savefig("generated_graph.png") #saves to a png file
+        plt.show() #displays it
         check_running = input("Make Another? ")
         if check_running[0].upper() != 'Y':
             print("Quitting...")
